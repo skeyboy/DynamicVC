7d632c7e8e6a47754f01ee550f2dcadda434d91e
sk <liyulong@yuelvhui.com>
2020-05-12 16:36:12 +0800

    动态framework制作

--- DynamicVC/DynamicVC.h
+++ DynamicVC/DynamicVC.h
@@ -15,5 +15,5 @@ FOUNDATION_EXPORT double DynamicVCVersionNumber;
 FOUNDATION_EXPORT const unsigned char DynamicVCVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DynamicVC/PublicHeader.h>
-
+#import <DynamicVC/DynamicViewController.h>
 
